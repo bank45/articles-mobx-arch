a67f2de4f91186c5bdf9033c128bed05668125a3
bank45 <bank45@yandex.ru>
2019-06-30 15:23:32 +0300

    webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,79 @@
+const path = require('path')
+const webpack = require('webpack')
+
+
 module.exports = {
-    entry: {
-        app: '.src/index.js'
-    },
+    mode: "development",
+    devtool: "inline-source-map",
+    devtool: false,
+    plugins: [
+      new webpack.SourceMapDevToolPlugin({})
+    ],
+    entry: "./src/index.tsx",
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist')
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
     },
     module: {
         rules: [
-            { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" }
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            {
+                test: /\.js$/,
+                exclude: ['/node_modules/', '/src/scss/'],
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.ts(x?)$/,
+                exclude: ['/node_modules/', '/src/scss/'],
+                use: [
+                    'babel-loader',
+                    'ts-loader',
+                ]
+            },
+            {
+                test:  /\.json$/,
+                loader: 'json-loader'
+            },
         ]
     }
-}
+};
+
+
+
+
+
+
+
+
+
+
+// module.exports = {
+//     mode: "development",
+//     context: path.resolve(__dirname, 'src'),
+//     entry: {
+//         app: ['./index.tsx']
+//     },
+//     output: {
+//         filename: 'bundle.js',
+//         path: path.resolve(__dirname, 'dist')
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.jsx$/,
+//                 exclude: /node_modules/,
+//                 use: [
+//                     {
+//                         loader: 'ts-loader'
+//                     }
+//                 ]
+//             }
+//         ]
+//     }
+// }
